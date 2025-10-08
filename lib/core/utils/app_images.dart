a01e58d94c53713fc31d5aed9dd10c509de7c80f
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-10-08 07:47:38 +0300

    add app image and icons

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,4 +1,106 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
+
+  /// Assets for images3users
+  /// assets/images/3users.svg
+  static const String images3users = "assets/images/3users.svg";
+
+  /// Assets for imagesAboutUs
+  /// assets/images/aboutUs.svg
+  static const String imagesAboutUs = "assets/images/aboutUs.svg";
+
+  /// Assets for imagesCalendar
+  /// assets/images/calendar.svg
+  static const String imagesCalendar = "assets/images/calendar.svg";
+
+  /// Assets for imagesCall
+  /// assets/images/call.svg
+  static const String imagesCall = "assets/images/call.svg";
+
+  /// Assets for imagesDolar Sign
+  /// assets/images/DolarSign.svg
+  static const String imagesDolarSign = "assets/images/DolarSign.svg";
+
+  /// Assets for imagesDownloadSuccess
+  /// assets/images/downloadSuccess.svg
+  static const String imagesDownloadSuccess =
+      "assets/images/downloadSuccess.svg";
+
+  /// Assets for imagesEmployee
+  /// assets/images/employee.svg
+  static const String imagesEmployee = "assets/images/employee.svg";
+
+  /// Assets for imagesExchange
+  /// assets/images/exchange.svg
+  static const String imagesExchange = "assets/images/exchange.svg";
+
+  /// Assets for imagesExchangeWithBackground
+  /// assets/images/exchangeWithBackground.svg
+  static const String imagesExchangeWithBackground =
+      "assets/images/exchangeWithBackground.svg";
+
+  /// Assets for imagesGithub
+  /// assets/images/github.svg
+  static const String imagesGithub = "assets/images/github.svg";
+
+  /// Assets for imagesGreenMoney
+  /// assets/images/greenMoney.svg
+  static const String imagesGreenMoney = "assets/images/greenMoney.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesLogin
+  /// assets/images/login.svg
+  static const String imagesLogin = "assets/images/login.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesNotFound
+  /// assets/images/notFound.svg
+  static const String imagesNotFound = "assets/images/notFound.svg";
+
+  /// Assets for imagesOnBoarding1
+  /// assets/images/onBoarding1.svg
+  static const String imagesOnBoarding1 = "assets/images/onBoarding1.svg";
+
+  /// Assets for imagesOnBoarding2
+  /// assets/images/onBoarding2.svg
+  static const String imagesOnBoarding2 = "assets/images/onBoarding2.svg";
+
+  /// Assets for imagesOnBoarding3
+  /// assets/images/onBoarding3.svg
+  static const String imagesOnBoarding3 = "assets/images/onBoarding3.svg";
+
+  /// Assets for imagesOutcome
+  /// assets/images/outcome.svg
+  static const String imagesOutcome = "assets/images/outcome.svg";
+
+  /// Assets for imagesProject
+  /// assets/images/project.svg
+  static const String imagesProject = "assets/images/project.svg";
+
+  /// Assets for imagesSearch
+  /// assets/images/search.svg
+  static const String imagesSearch = "assets/images/search.svg";
+
+  /// Assets for imagesSignup
+  /// assets/images/signup.svg
+  static const String imagesSignup = "assets/images/signup.svg";
+
+  /// Assets for imagesSplash
+  /// assets/images/splash.svg
+  static const String imagesSplash = "assets/images/splash.svg";
+
+  /// Assets for imagesTheme
+  /// assets/images/theme.svg
+  static const String imagesTheme = "assets/images/theme.svg";
+
+  /// Assets for imagesUser
+  /// assets/images/user.svg
+  static const String imagesUser = "assets/images/user.svg";
 }
