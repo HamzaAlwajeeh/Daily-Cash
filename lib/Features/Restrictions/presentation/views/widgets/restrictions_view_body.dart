6f2b47a4d12742ba2f1269b00c0954d02bd0d270
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-11-07 06:15:50 +0300

    EditCreate RestrictionView

--- lib/Features/Restrictions/presentation/views/widgets/restrictions_view_body.dart
+++ lib/Features/Restrictions/presentation/views/widgets/restrictions_view_body.dart
@@ -1,11 +1,8 @@
-import 'package:daily_cash/Features/Restrictions/data/models/restrictions_model.dart';
-import 'package:daily_cash/Features/Restrictions/data/restrictions_data.dart';
-import 'package:daily_cash/core/utils/app_colors.dart';
+import 'package:daily_cash/Features/Restrictions/presentation/views/widgets/restrictions_list_view.dart';
 import 'package:daily_cash/core/utils/app_images.dart';
 import 'package:daily_cash/core/utils/app_text_style.dart';
 import 'package:daily_cash/core/widgets/custom_text_feild.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class RestrictionsViewBody extends StatelessWidget {
   const RestrictionsViewBody({super.key});
@@ -24,125 +21,6 @@ class RestrictionsViewBody extends StatelessWidget {
           ),
           Expanded(child: RestrictionsListView()),
         ],
-      ),
-    );
-  }
-}
-
-class RestrictionsListView extends StatefulWidget {
-  const RestrictionsListView({super.key});
-
-  @override
-  State<RestrictionsListView> createState() => _RestrictionsListViewState();
-}
-
-class _RestrictionsListViewState extends State<RestrictionsListView> {
-  List<RestrictionsModel> restrictions = [];
-  @override
-  void initState() {
-    restrictions = getRestrictions();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      physics: BouncingScrollPhysics(),
-      itemCount: restrictions.length,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: EdgeInsets.symmetric(vertical: 8),
-          child: RestrictionsItem(restrictions: restrictions[index]),
-        );
-      },
-    );
-  }
-}
-
-class RestrictionsItem extends StatelessWidget {
-  const RestrictionsItem({super.key, required this.restrictions});
-  final RestrictionsModel restrictions;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: AppColors.textFeilColor,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SvgPicture.asset(Assets.imagesExchangeWithBackground),
-            const SizedBox(width: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 5,
-              children: [
-                Text.rich(
-                  textAlign: TextAlign.center,
-                  style: TextStyles.bold16,
-                  TextSpan(
-                    text: 'من حساب ',
-                    children: [
-                      TextSpan(
-                        text: restrictions.fromPerson,
-                        style: TextStyles.bold16.copyWith(
-                          color: AppColors.customRed,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Text.rich(
-                  textAlign: TextAlign.center,
-                  style: TextStyles.bold16,
-                  TextSpan(
-                    text: 'الى حساب ',
-                    children: [
-                      TextSpan(
-                        text: restrictions.toPerson,
-                        style: TextStyles.bold16.copyWith(
-                          color: AppColors.green,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Text(
-                  restrictions.description,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyles.regular14,
-                ),
-                Text(
-                  restrictions.date,
-                  style: TextStyles.bold14.copyWith(
-                    color: AppColors.textSecondaryColor.withOpacity(0.5),
-                  ),
-                ),
-              ],
-            ),
-            Spacer(),
-            Container(
-              width: 103,
-              height: 40,
-              decoration: BoxDecoration(
-                color: AppColors.primaryColor.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Center(
-                child: Text(
-                  restrictions.amount.round().toString(),
-                  style: TextStyles.bold18,
-                ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
