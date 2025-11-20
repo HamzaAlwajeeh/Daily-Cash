e4494543bbfaaf8b93b578a0d9f5e74259a29e2e
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-11-20 13:38:15 +0300

    Implement user registration with API integration and error handling

--- lib/Features/auth/data/repos/auth_repo_impl.dart
+++ lib/Features/auth/data/repos/auth_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:daily_cash/Features/auth/data/repos/auth_repo.dart';
 import 'package:daily_cash/core/errors/failuar.dart';
 import 'package:daily_cash/core/services/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class AuthRepoImpl implements AuthRepo {
   ApiService apiService;
@@ -20,19 +21,19 @@ class AuthRepoImpl implements AuthRepo {
     String email,
     String password,
   ) async {
-    // try {
-    //   var data = await apiService.post(
-    //     endPoint: 'register',
-    //     body: {'name': name, 'email': email, 'password': password},
-    //     token: null,
-    //   );
-    //   User user = User.fromJson(data['user']);
-    //   return Right(user);
-    // } catch (e) {
-    //   if (e is DioException) {
-    //     return left(ServerFailure.fromDioException(e));
-    //   }
-    //   return left(ServerFailure(errorMessage: e.toString()));
-    // }
+    try {
+      var data = await apiService.post(
+        endPoint: 'register',
+        body: {'name': name, 'email': email, 'password': password},
+        token: null,
+      );
+      User user = User.fromJson(data['user']);
+      return Right(user);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
   }
 }
