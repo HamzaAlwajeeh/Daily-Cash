f24b6f8ae5e435ac8bd325dd3d3375e6c1f829a6
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-11-26 08:27:05 +0300

    Refactor HomeView and RecentOperations to use BLoC for state management

--- lib/Features/home/presentation/views/home_view.dart
+++ lib/Features/home/presentation/views/home_view.dart
@@ -1,11 +1,24 @@
+import 'package:daily_cash/Features/home/presentation/controller/get_all_operation_cubit/get_all_operation_cubit.dart';
 import 'package:daily_cash/Features/home/presentation/views/add_operation_view.dart';
 import 'package:daily_cash/Features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:daily_cash/core/widgets/custom_floating_action_buttomn.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
   static const String routeName = '/home';
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  void initState() {
+    BlocProvider.of<GetAllOperationsCubit>(context).getAllOperationss();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/Features/home/presentation/views/widgets/recent_operations.dart
+++ lib/Features/home/presentation/views/widgets/recent_operations.dart
@@ -1,40 +1,51 @@
 import 'package:daily_cash/Features/home/data/models/operation.dart';
+import 'package:daily_cash/Features/home/presentation/controller/get_all_operation_cubit/get_all_operation_cubit.dart';
+import 'package:daily_cash/Features/home/presentation/controller/get_all_operation_cubit/get_all_operation_state.dart';
 import 'package:daily_cash/Features/home/presentation/views/widgets/no_recent_operatios_widget.dart';
 import 'package:daily_cash/Features/home/presentation/views/widgets/recent_operations_list_view.dart';
+import 'package:daily_cash/core/helper/custom_loading_indicator.dart';
 import 'package:daily_cash/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RecentOperations extends StatelessWidget {
   const RecentOperations({super.key});
   @override
   Widget build(BuildContext context) {
-    final List<Operation> operations = [];
-    final topTen =
-        operations.length > 10
-            ? operations.sublist(operations.length - 10)
-            : operations;
-
-    return Expanded(
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: const Text(
-                'اخر العمليات',
-                style: TextStyles.bold16,
-                textAlign: TextAlign.start,
-              ),
+    return BlocBuilder<GetAllOperationsCubit, GetAllOperationsState>(
+      builder: (context, state) {
+        List<Operation> operations =
+            BlocProvider.of<GetAllOperationsCubit>(context).operations;
+        // ناخذ اخر عشر عمليات ونقلبها
+        List<Operation> topTen = operations.reversed.take(10).toList();
+        return Expanded(
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: const Text(
+                    'اخر العمليات',
+                    style: TextStyles.bold16,
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+                const SizedBox(height: 12),
+                if (state is GetAllOperationsFailure)
+                  Center(child: Text(state.errorMessage))
+                else if (state is GetAllOperationsLoading)
+                  CustomLoadingIndicator()
+                else if (operations.isEmpty)
+                  NoRecentOperatiosWidget()
+                else
+                  RecentOperationsListView(operations: topTen),
+              ],
             ),
-            const SizedBox(height: 12),
-            operations.isEmpty
-                ? NoRecentOperatiosWidget()
-                : RecentOperationsListView(operations: topTen),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
